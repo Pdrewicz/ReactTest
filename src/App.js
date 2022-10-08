7514793b4a202d0915505c03406f20c34937d6df
Pdrewicz <106173218+Pdrewicz@users.noreply.github.com>
2022-10-08 17:55:34 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { useState} from 'react';
 import './App.css';
+import { createContext } from 'react';
+
+export const ThemeContext = createContext("light");
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [theme, setTheme] = useState("light");
+
+  function toggleTheme() {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
 
   return (
-    <div className="App">
-      <button onClick={() => setCounter(counter-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter(counter+1)}>+</button>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className="App" id={theme}>
+        <div id='counter'>
+          <button onClick={() => setCounter(counter-1)}>-</button>
+          <h1>{counter}</h1>
+          <button onClick={() => setCounter(counter+1)}>+</button>
+        </div>
+        <button id='themeToggle' onClick={() => toggleTheme()}><h1>Toggle Theme</h1></button>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
